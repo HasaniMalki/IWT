b411d41e6be52837a9f77faa6974685ea176289b
Supun84 <83086115+Supun84@users.noreply.github.com>
2021-05-31 22:38:46 +0530

    Add files via upload

---
+++ profile/index.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <div class="container">
+       <div class="main">
+           <div class="tobar">
+               <a href="">logout</a>
+               <a href="">support</a>
+               <a href="">work</a>
+               <a href="">home</a>
+           </div>
+           <div class="row">
+               <div class="col-md-4-mt-">
+                   <div class="card text-center sidebar">
+                       <div class="card-body">
+                           <img src="image.jpg" class="rounded-cercle" width="150">
+                           <div class="mt-3">
+                               <h3>burk macklin</h3>
+                               <a href="">home</a>
+                               <a href="">work</a>
+                               <a href="">support</a>
+                               <a href="">setting</a>
+                               <a href="">signout</a>
+                           </div>
+                       </div>
+                   </div>                           
+               </div>
+               <div class="col-md-8 mt-1">
+                   <div class="card mb-3 content">
+                       <h1 class="m-3 pt-4">about</h1>
+                       <div class="card body">
+                           <div class="row">
+                               <div class="col-md-3">
+                                   <h5> Full Name</h5> 
+                               </div>
+                               <div class="col-md-9 text-secondary">
+                                   Burt macklin
+                               </div>
+                           </div>
+                           <hr>
+                           <div class="row">
+                               <div class="col-md-3">
+                                   <h5>email</h5>
+                               </div>
+                               <div class="col-md-9 text-secondary">
+                                   abc@gmail.com
+                               </div>
+                           </div>
+                       </div>
+                       <hr>
+                       <div class="col-md-3">
+                           <h5>phone</h5>
+                       </div>
+                       <div class="col-md-10 text secondary">
+                           0775687123
+                       </div>
+                   </div>
+               <div class="row">
+                   <div class="col-md-3">
+                       <h5>Address</h5>
+                   </div>
+                   <div class="col-md-9 text-secondary">
+                       street no. 4, xyz
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div class="card mb-3 content">
+           <h1 class="m-3">Rec1ent Projects</h1>
+           <div class="card-body">
+               <div class="row">
+                   <div class="col-md-3">
+                       <h5>Projects Name</h5>
+                   </div>
+                   <div class="col-md-9 text-secondary">
+                       Project
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>  
+</body>
+ </html>
